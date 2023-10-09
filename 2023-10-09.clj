d4164f6e0b49d74bdd8a95143c714885faf73216
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-10-09 21:19:28 +0530

    2023-10-09

--- 2023-10-09.clj
+++ 2023-10-09.clj
@@ -5,38 +5,57 @@
 
 ;; ------------- Parse words -------------
 
-(def places ["Million" "Thousand" "Hundred" "Base"])
+;; key order matters for the algorithm to work
+(def places (array-map "Million" 1000000 "Thousand" 1000 "Hundred" 100 "Base" 1))
+
+(declare add-to-map)
+(defn- place-words-map [words-seq]
+  (->> (keys places)
+       (reduce add-to-map [{} words-seq])
+       (first)))
 
 (defn- add-to-map [[places-map word-seq] place]
   (cond
     (= place "Base")               [(assoc places-map place word-seq) []]
     (some #{place} word-seq)       (let [[place-words [_ & remaining-words]] (split-with (complement #{place}) word-seq)]
-                                     [(assoc places-map place place-words) remaining-words])
+                                     [(assoc places-map place (place-words-map place-words)) remaining-words])
     :else                          [(assoc places-map place nil) word-seq]))
-
-(defn- place-words-map [words]
-  (->> places
-       (reduce add-to-map [{} (s/split words #" ")])
-       (first)))
 
 ;; ------------- Convert words to numeral -------------
 
-;; Each key of the places-words-map could have one or two words.
+;; Each key of the places-words-map is a sub-map of the same structure, i.e. it is recursive, except for the "Base" key
+;; The base value has one or two words.
 ;; If two words, then the first word is a "ty" i.e. twenty, thirty, etc. The second word is a unit i.e. two, three, etc
 ;; If one word, then it could be a "unit" or it could be a "teen" i.e. "ten", "eleven", "twelve", "thirtee" etc
-;; units and teens are mapped to numbers
 ;; tys are mapped to numbers multiplied by 10
+;; units and teens are mapped to numbers
 (def ty->num {"Twenty" 20 "Thirty" 30 "Forty" 40 "Fifty" 50 "Sixty" 60 "Seventy" 70 "Eighty" 80 "Ninety" 90})
-(def unit->num {"One" 1 "Two" 2 "Three" 3 "Four" 4 "Five" 5 "Six" 6 "Seven" 7 "Eight" 8 "Nine" 9})
 (def teen->num {"Ten" 10 "Eleven" 11 "Twelve" 12 "Thirteen" 13 "Fourteen" 14 "Fifteen" 15 "Sixteen" 16 "Seventeen" 17 "Eighteen" 18 "Nineteen" 19})
+(def unit->num {"One" 1 "Two" 2 "Three" 3 "Four" 4 "Five" 5 "Six" 6 "Seven" 7 "Eight" 8 "Nine" 9})
+(defn base->num [[f s]]
+  (if (nil? s)
+    (or (teen->num f)
+        (unit->num f))
+    (+ (ty->num f)
+       (unit->num s))))
+
+(defn places-to-number [places-map]
+  (if-not places-map
+    0
+    (->> (dissoc places "Base")
+         (map (fn [[place mul]] (* mul (places-to-number (get places-map place)))))
+         (cons (base->num (get places-map "Base")))
+         (reduce +))))
 
 ;; ------------- Main -------------
 
 (defn parse-number [words]
-  (place-words-map words))
+  (places-to-number (place-words-map (s/split words #" "))))
 
-(assert (= 45045 (parse-number "Forty Five Thousand Forty Five")))
-(assert (= 12345 (parse-number "Twelve Thousand Three Hundred Forty Five")))
-(assert (= 16503 (parse-number "Twelve Thousand Forty Five Hundred Three")))
-(assert (= 16503 (parse-number "Sixteen Thousand Five Hundred Three")))
-(assert (= 123 (parse-number "One Hundred Twenty Three")))
+(assert (= 123 (parse-number "One Hundred Twenty Three")))  ;; simple
+(assert (= 12345 (parse-number "Twelve Thousand Three Hundred Forty Five")))  ;; simple
+(assert (= 45045 (parse-number "Forty Five Thousand Forty Five")))  ;; multiple parts in thousands place
+(assert (= 16503 (parse-number "Sixteen Thousand Five Hundred Three")))  ;; simple
+(assert (= 16503 (parse-number "Twelve Thousand Forty Five Hundred Three")))  ;; unconventional wording, equal to the above simple case
+(assert (= 345035 (parse-number "Three Hundred Forty Five Thousand Thirty Five")))  ;; hundreds within thousands place
+(assert (= 345000035 (parse-number "Three Hundred Forty Five Million Thirty Five")))  ;; hundreds within millions place
