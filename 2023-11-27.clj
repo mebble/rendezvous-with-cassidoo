7f877d69bcbd68d793dec7786a8f697cbb93c19c
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-11-29 16:27:27 +0530

    2023-11-27

---
+++ 2023-11-27.clj
@@ -0,0 +1,25 @@
+;; https://buttondown.email/cassidoo/archive/when-i-let-go-of-what-i-am-i-become-what-i-might/
+;; https://twitter.com/cassidoo/status/1729035353829630183
+
+;; Run this with:
+;; bb --classpath "$(clojure -Spath -Sdeps '{:deps {org.clojure/math.combinatorics {:mvn/version "0.2.0"}}}')" 2023-11-27.clj
+
+(require '[clojure.math.combinatorics :as combo])
+
+(defn combo-price [items-combo]
+  (->> items-combo
+       (map :price)
+       (reduce +)))
+
+(defn min-cost-for-calories [calories prices daily-goal]
+  (let [items (map (fn [c p] {:calories c :price p})
+                   calories
+                   prices)]
+    (->> items
+         (combo/subsets)
+         (filter (fn [combo] (>= (reduce + (map :calories combo))
+                                 daily-goal)))
+         (map combo-price)
+         (apply min))))
+
+(assert (= 160 (min-cost-for-calories [200, 400, 600, 800] [50, 60, 80, 100] 1200)))
