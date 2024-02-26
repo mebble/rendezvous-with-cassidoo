893c9ee7b6b7d30107e80cf4ca240b034f2f7d08
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-02-26 18:38:25 +0530

    2024-02-26

---
+++ 2024-02-26.clj
@@ -0,0 +1,27 @@
+;; https://buttondown.email/cassidoo/archive/the-potential-for-greatness-lives-within-each-of/
+;; https://twitter.com/cassidoo/status/1762023522241573185
+
+(require '[clojure.string :refer [index-of]])
+
+(defn- all-indexes [string value]
+  (loop [indexes (vector)
+         from 0]
+    (if-let [i (index-of string value from)]
+      (recur (conj indexes i)
+             (inc i))
+      indexes)))
+
+(defn remove-digit [n d]
+  (let [s   (str n)
+        indexes (all-indexes s (str d))]
+    (if (seq indexes)
+      (->> indexes
+           (map (fn [i] (str (subs s 0 i)
+                             (subs s (inc i)))))
+           (map parse-long)
+           (apply max))
+      n)))
+
+(assert (= 3415926 (remove-digit 31415926 1)))
+(assert (= 231 (remove-digit 1231 1)))
+(assert (= 111 (remove-digit 111 2)))
