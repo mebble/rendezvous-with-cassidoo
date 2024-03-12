4ca019dac2553afd7a362a46d02d27486f1cc885
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-03-12 19:55:35 +0530

    2024-03-11 improved

--- 2024-03-11.clj
+++ 2024-03-11.clj
@@ -1,11 +1,19 @@
 ;; https://buttondown.email/cassidoo/archive/if-you-dont-go-towards-the-thing-you-fear-you/
 ;; https://twitter.com/cassidoo/status/1767084127541449018
 
+(defn- seq-or [x xs]
+  (if (seq xs)
+    xs
+    x))
+
 (defn max-gap [xs]
   (let [sorted (sort xs)]
     (->> (rest sorted)
          (map vector sorted)
          (map (fn [[x y]] (- y x)))
+         (seq-or [0])
          (apply max))))
 
 (assert (= 3 (max-gap [3 6 9 1 2])))
+(assert (= 0 (max-gap [1])))
+(assert (= 0 (max-gap [])))
